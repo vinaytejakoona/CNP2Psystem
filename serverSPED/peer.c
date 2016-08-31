88e960931b462268a3e2f634306cb1788915228a
vinaytejakoona <vinaytejakoona@gmail.com>
2016-08-31 19:38:27 +0530

    Update peer.c
    send file, search

--- serverSPED/peer.c
+++ serverSPED/peer.c
@@ -1,5 +1,5 @@
 /*
- ** client.c -- a stream socket client demo
+ peer
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,6 +25,48 @@
     }
     return &(((struct sockaddr_in6*) sa)->sin6_addr);
 }
+
+
+int recv_file(int sock, char * file_name)
+{
+    char send_str [MAX_SEND_BUF];/* message to be sent to server*/
+    int f; /* file handle for receiving file*/
+    ssize_t sent_bytes, rcvd_bytes, rcvd_file_size;
+    int recv_count;/* count of recv() calls*/
+    char recv_str[MAX_RECV_BUF]; /* buffer to hold received data */
+    size_t send_strlen; /* length of transmitted string */
+    sprintf(send_str, "%s \n", file_name); /* add CR/LF (new line) */
+    send_strlen = strlen(send_str); /* length of message to be transmitted */
+    if( (sent_bytes = send(sock, file_name, send_strlen, 0)) <0)
+    {
+            perror("send error");
+                        return -1;
+    }/* attempt to create file to save received data. 0644 = rw-r--r-- */
+    if( (f = open(file_name, O_WRONLY|O_CREAT,0644)) < 0)
+    {
+        perror("error creating file");
+        return -1;
+    }
+    recv_count = 0; /* number of recv() calls required to receive the file */
+    rcvd_file_size = 0; /* size of received file */
+    
+    /* continue receiving until ? (data or close) */
+    while( (rcvd_bytes = recv(sock, recv_str, MAX_RECV_BUF, 0)) > 0)
+    {
+        recv_count++;
+        rcvd_file_size += rcvd_bytes;
+        if(write(f, recv_str, rcvd_bytes) < 0)
+        { 
+            perror("error writing to file");
+            return -1;
+        }
+    }
+    close(f); /* close file*/
+    printf("Client Received: %d bytes in %d recv(s) \n", rcvd_file_size, recv_count);
+    return rcvd_file_size;
+}
+
+
 
 int main(int argc, char *argv[]) {
 
